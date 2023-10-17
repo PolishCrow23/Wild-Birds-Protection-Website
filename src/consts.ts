ca219603d9a3dad452ee85ea623d5476814ffe32
PolishCrow23 <igaspodzieja00@gmail.com>
2023-10-17 19:25:55 +0200

    new design

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'ZAKAZ KOLCÓW';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Wspólnie chronimy ptaki w mieście';
